cb7c1a2606566aed62d5082f45098c71c12d4410
dipace7 <codeict.dipesh@gmail.com>
2024-10-11 16:23:05 +0545

    larger fonts

--- src/components/landing/what-we-do/WhatWeDo.tsx
+++ src/components/landing/what-we-do/WhatWeDo.tsx
@@ -1,10 +1,17 @@
-import { Bot, Mail, BarChart3 } from "lucide-react";
+import {
+  Bot,
+  Mail,
+  BarChart3,
+  UsersRound,
+  HandHeart,
+  Goal,
+} from "lucide-react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const FeatureCard = ({ icon: Icon, title, description }: any) => (
   <div className="bg-slate-800 rounded-lg p-6 transition-all duration-300 hover:bg-slate-700 hover:shadow-lg">
-    <Icon className="text-yellow-400 mb-4" size={32} />
-    <h3 className="text-2xl font-semibold mb-2">{title}</h3>
+    <Icon className="text-yellow-400 mb-4" size={64} />
+    <h3 className="text-3xl font-semibold mb-4">{title}</h3>
     <p className="text-slate-300 text-xl">{description}</p>
   </div>
 );
@@ -30,19 +37,19 @@ const WhatWeDo = () => {
         "Unlock the full potential of your customer data with our comprehensive insights dashboard. Gain a deeper understanding of each user through personalized analytics, tracking their journey from first contact to repeat purchases. Our AI-powered system identifies trends, predicts future behaviors, and provides actionable insights to help you make data-driven decisions that grow your business.",
     },
     {
-      icon: BarChart3,
+      icon: UsersRound,
       title: "Smart Lead Capturing",
       description:
         "AI-powered forms and chatbots ensure every opportunity is captured by tracking customer interactions across all touchpoints, from your website to social media. These tools are designed to engage customers in real time, ensuring no lead slips through the cracks. By analyzing customer behavior and adapting dynamically, our AI ensures that interactions remain relevant and personalized at every stage, enhancing the chances of conversion. Whether responding to inquiries, guiding users through forms, or offering assistance, the system works seamlessly to maximize lead generation and deliver tailored experiences that drive engagement.",
     },
     {
-      icon: BarChart3,
+      icon: Goal,
       title: "Engagement Automation",
       description:
         "Our engagement automation platform keeps customers actively connected with your brand through timely, personalized communications that anticipate their needs based on real-time data and behavioral insights. Regular check-ins ensure consistent interaction, while tailored offers provide relevant promotions that resonate with individual customer journeys, boosting engagement and conversion rates. Feedback loops capture customer insights at key moments, allowing continuous improvement in communications and personalized experiences. This automated process creates a seamless, ongoing relationship with customers, fostering loyalty, increasing satisfaction, and driving long-term business growth.",
     },
     {
-      icon: BarChart3,
+      icon: HandHeart,
       title: "Personalized Lead Nurturing And Loyalty Programs",
       description:
         "Our loyalty programs are designed to create lifetime customers. Automate rewards based on customer activity, birthdays, anniversaries, and more. Reward programs are fully customizable and easy to manage, giving customers more reasons to return.Our loyalty programs are designed to create lifetime customers. Automate rewards based on customer activity, birthdays, anniversaries, and more. Reward programs are fully customizable and easy to manage, giving customers more reasons to return.",
